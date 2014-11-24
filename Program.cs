1dc492d633ef9f45a31ba4103f0fba4c49716b7a
webley <webley@users.noreply.github.com>
2014-11-24 08:09:57 +0000

    Hello?

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User {Username = "dave", FirstName = "Dave", LastName = "Awesome", PotatoScore = 99};
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace potato
+{
+    /// <summary>
+    /// Represents a potato-user.  One who uses potatoes.
+    /// </summary>
+    public class User
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PotatoScore { get; set; }
+    }
+}
